f0132f0c1e137dfec5bce70034dad599f817edf5
dhruvgupta23 <144788153+dhruvgupta23@users.noreply.github.com>
2023-09-13 16:51:01 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let state = null;
+
+//datatypes
+
+// number
+// boolean => true/false
+// string => ""
+//null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof state);
+console.log(typeof undefined);
